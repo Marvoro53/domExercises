e6d2bf369401f2f894a5c9528c1097d2ab5452fc
Marvoro53 <marvinlara321@gmail.com>
2021-01-27 14:46:06 -0600

    added exserices

--- domExercises.js
+++ domExercises.js
@@ -12,15 +12,13 @@ LiTags.forEach(function(x) {
 
 
 // 3. Create an image tag, set its `src` attribute to `http://49.media.tumblr.com/tumblr_m6qt1rjPSz1rxjzkho1_500.gif`, and append the to the `#greeting` div.
-var myPicture= 'http://49.media.tumblr.com/tumblr_m6qt1rjPSz1rxjzkho1_500.gif';
+var myImage= 'http://49.media.tumblr.com/tumblr_m6qt1rjPSz1rxjzkho1_500.gif';
 var image = new Image();
-image.src = myPicture;
-// Parent node is body, child is (myPic)
+image.src = myImage;
 document.getElementById('greeting').appendChild(image);
 
-
-
-
+var img= document.createElement('img');
+img.src= "http://49.media.tumblr.com/tumblr_m6qt1rjPSz1rxjzkho1_500.gif"
 // 4. Create and add a ul element to the end of the body with a class of "todo-items" 
 // 5. Go through the array `['make coffee','eat donut','change diapers','drive to work']` and create an li element for each item e.g. `<li>make coffee</li>`
 let list = ['Make Coffee', 'Eat Donut', 'Change Diapers', 'Drive To Work'];
@@ -41,10 +39,22 @@ console.log(todoItems);
 
 // 4. Add the class of `selected` to an `<li>` when it is clicked. Remove it from any other `li`s as well. 
 
+var selector, elems, makeActive;
+selector = "li";
+elems = document.getElementsByTagName(selector);
 
+makeActive = function (e) {
+    for (var i = 0; i < elems.length; i++){
+        elems[i].classList.remove('selected');
+      }
+    e.target.classList.add('selected');
+};
+
+for (var i = 0; i < elems.length; i++){
+    elems[i].addEventListener('mousedown', makeActive);
+}
 
 // 5. Change the image to be the most recently clicked food item.
-
 
 // 6. When the gray div is moused over, it's removed from the DOM.
 function mouseOver() {
@@ -67,7 +77,37 @@ function normalWidth() {
 
 // 9. When the 1, 2, 3, 4, 5, 6, 7, 8, 9, or 0 key is pressed, the page alerts the message "I HATE NUMBERZZZ!"
 
-
-// BONUS: If someone types the [Konami Code](https://en.wikipedia.org/wiki/Konami_Code), the page alerts "YOU ARE AN EVENT HANDLER GURUUUUUUUUU!"
-
-
+document.onkeydown = function(numbers) {
+  switch (numbers.keyCode) {
+      case 48:
+          alert('I HATE NUMBERZZZ!');
+          break;
+      case 49:
+          alert('I HATE NUMBERZZZ!');
+          break;
+      case 50:
+          alert('I HATE NUMBERZZZ!');
+          break;
+      case 51:
+          alert('I HATE NUMBERZZZ!');
+          break;
+      case 52:
+          alert('I HATE NUMBERZZZ!');
+          break;
+      case 53:
+          alert('I HATE NUMBERZZZ!');
+          break;
+      case 54:
+          alert('I HATE NUMBERZZZ!');
+          break;
+      case 55:
+          alert('I HATE NUMBERZZZ!');
+          break;
+      case 56:
+          alert('I HATE NUMBERZZZ!');
+          break;
+      case 57:
+          alert('I HATE NUMBERZZZ!');
+          break;
+  }
+};
